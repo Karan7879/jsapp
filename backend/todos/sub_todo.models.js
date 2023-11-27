8e656d830751a2a3447a947df99a1f4f2654ecd1
Karan7879 <40995972+Karan7879@users.noreply.github.com>
2023-11-27 18:11:17 +0000

    Few other todo changes

--- backend/todos/sub_todo.models.js
+++ backend/todos/sub_todo.models.js
@@ -1,32 +1,10 @@
 import mongoose from "mongoose"
 
-const userSchema = new mongoose.Schema(
-   
-    {
-    username:{
-        type:String,
-        required: true,
-        unique: true,
-        lowercase : true
-    },
-
-    emailId:{
-        type:String,
-        required: true,
-        unique:true,
-        lowercase:true
-    },
-
-    password:{
-         type:String,
-         required : true
-    }
+const subtodoSchema = new mongoose.Schema({
+    
 },{timestamps:true})
 
-export const User = mongoose.model("User",userSchema)
-
-// User will save as users User -> users
-
+export const Subtodo = mongoose.model("Subtodo",subtodoSchema)
 
 
 
---
+++ backend/todos/user.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+   
+    {
+    username:{
+        type:String,
+        required: true,
+        unique: true,
+        lowercase : true
+    },
+
+    emailId:{
+        type:String,
+        required: true,
+        unique:true,
+        lowercase:true
+    },
+
+    password:{
+         type:String,
+         required : true
+    }
+},{timestamps:true})
+
+export const User = mongoose.model("User",userSchema)
+
+// User will save as users User -> users
+
+
+
+
+
+
