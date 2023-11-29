9e384b75d51290b7d4048f60af3ca618ca16f892
Karan7879 <40995972+Karan7879@users.noreply.github.com>
2023-11-29 15:49:41 +0000

    Function fnished

--- start/test.js
+++ start/test.js
@@ -1,1 +1,19 @@
-console.log("get starte")
+const user = {
+    name:'karan',
+    taskq:'coder'
+}
+
+function handleObjects(anyobjects){
+    console.log(`username is ${anyobjects.name} and task is ${anyobjects.task}`)
+
+}
+
+
+handleObjects(user)
+
+myar = [1,2,3,4,]
+
+function seconsa(arr){
+    console.log(arr[1])
+}
+seconsa(myar)
